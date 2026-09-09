dce1247eb6fc9b4bfd8a40a194784e1e1c5d9d35
seud4rk <suporte.seudark@gmail.com>
2026-09-09 08:45:41 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ document.querySelector('#app').innerHTML = `
       <h1><span>Screen</span>City</h1>
       <p class="subtitle">Aplicativo oficial de mídia indoor</p>
 
-      <a class="download" href="/ScreenCity.apk" download>
+      <a class="download" href="/ScreenCity-1.2.apk" download>
         BAIXAR SCREENCITY
       </a>
 
